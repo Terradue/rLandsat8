4e0141dbfa504765f18adbce020d1b27f2de7508
Fabrice Brito <fabrice.brito@terradue.com>
2014-09-03 07:03:51 +0200

    Update ToNDVI.R

--- src/main/R/rLandsat8/R/ToNDVI.R
+++ src/main/R/rLandsat8/R/ToNDVI.R
@@ -1,5 +1,5 @@
 #' creates a raster with the NDVI vegetation index
-#' @description Creates a raster with the with the LSWI vegetation index: NDVI=(ρNIR −ρRed)/(ρNIR +ρRed)
+#' @description Creates a raster with the with the NDVI vegetation index: NDVI=(ρNIR −ρRed)/(ρNIR +ρRed)
 #'
 #' @param product name of the product, e.g. LC80522102014165LGN00. It must be in the working directory.
 #' @return brightness temperature raster
