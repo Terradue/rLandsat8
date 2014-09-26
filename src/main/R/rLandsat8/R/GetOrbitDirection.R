2b55f5bc1bd53cf3d35982f53e2f87b1fc433a7a
Costanzo <costanzo.d@gmail.com>
2014-09-26 15:38:55 +0200

    more function
    add GetOrbitDirection
    fixed ToRGB

--- src/main/R/rLandsat8/R/GetOrbitDirection.R
+++ src/main/R/rLandsat8/R/GetOrbitDirection.R
@@ -1,7 +1,7 @@
 #' @description Retrieves ascending/descending orbit direction information from the product
 #'
 #' @param list with metadata and raster bands
-#' @return ascending/descending
+#' @return ascending/descending or NA if information not found
 #' @examples \dontrun{
 #' filename<-"LC81880342014238LGN00"
 #' my.data <- ReadLandsat8(filename)
@@ -10,5 +10,8 @@
 #'
 #' @export
 GetOrbitDirection<-function(product){
-     
+     direction <- as.character(wrs2$MODE[wrs2$PATH == product$metadata$wrs_path & wrs2$ROW == product$metadata$wrs_row])
+     if(length(direction)==0)
+          return(NA)
+     return ( direction )     
 }
--- src/main/R/rLandsat8/R/ToRGB.R
+++ src/main/R/rLandsat8/R/ToRGB.R
@@ -16,7 +16,7 @@
 ToRGB<-function(red.component, green.component, blue.component){
      # landsat 8 properties
      landsat8.bitdepth <- 16
-     landsat8.colourlevel <- 2 ^ landsat8.bitdepth
+     landsat8.colourlevel <- 2 ^ landsat8.bitdepth - 1
      mapping.landasta8colourlevel <- 255 / landsat8.colourlevel
      
      # check band components compatibilities
@@ -24,11 +24,24 @@ ToRGB<-function(red.component, green.component, blue.component){
           stop("The 3 band components must have the same dimension!")
      
      # any r/g/b component is suitable for the setting operation
+     rgb.datasize <- red.component@ncols * red.component@nrows
      GT   <- GridTopology(c(0, 0), c(1, 1), c(red.component@ncols, red.component@nrows))
-     SGDF <- SpatialGridDataFrame(GT, data=data.frame(red=runif(red.component@ncols * red.component@nrows)))
+     SGDF <- SpatialGridDataFrame(GT, data=data.frame(red = rep(0,times=rgb.datasize), green = rep(0,times=rgb.datasize), blue = rep(0,times=rgb.datasize)))
      
-     SGDF$red   <- red.component@data@values   * mapping.landasta8colourlevel
-     SGDF$green <- green.component@data@values * mapping.landasta8colourlevel
-     SGDF$blue  <- blue.component@data@values  * mapping.landasta8colourlevel
+     data.red <- getValues(red.component) * mapping.landasta8colourlevel
+     data.red[data.red < 0] <- 0
+     data.red[data.red > 255] <- 255
+     SGDF$red   <- data.red   * mapping.landasta8colourlevel
+     
+     data.green <- getValues(green.component) * mapping.landasta8colourlevel
+     data.green[data.green < 0] <- 0
+     data.green[data.green > 255] <- 255
+     SGDF$green <- data.green
+     
+     data.blue <- getValues(blue.component) * mapping.landasta8colourlevel
+     data.blue[data.blue < 0] <- 0
+     data.blue[data.blue > 255] <- 255
+     SGDF$blue  <- data.blue
+     #image(SGDF, red="red", green="green", blue="blue")
      return(raster(SGDF))
 }
