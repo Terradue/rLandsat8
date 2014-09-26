b5865ba3d60024fe0f363c312b71b2ba624a8e64
Costanzo <costanzo.d@gmail.com>
2014-09-26 16:59:46 +0200

    test
    test

--- src/main/R/rLandsat8/R/GetOrbit.R
+++ src/main/R/rLandsat8/R/GetOrbit.R
@@ -1,15 +1,15 @@
 #' @description Retrieves ascending/descending orbit direction information from the product
 #'
 #' @param list with metadata and raster bands
-#' @return ascending/descending or NA if information not found
+#' @return [A]scending/[D]escending or NA if information not found
 #' @examples \dontrun{
 #' filename<-"LC81880342014238LGN00"
 #' my.data <- ReadLandsat8(filename)
-#' GetOrbitDirection(my.data)
+#' GetOrbit(my.data)
 #' }
 #'
 #' @export
-GetOrbitDirection<-function(product){
+GetOrbit<-function(product){
      direction <- as.character(wrs2$MODE[wrs2$PATH == product$metadata$wrs_path & wrs2$ROW == product$metadata$wrs_row])
      if(length(direction)==0)
           return(NA)
