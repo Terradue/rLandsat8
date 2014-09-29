d78e3f96d64bc558e870306ca72d450e30a39154
Costanzo <costanzo.d@gmail.com>
2014-09-29 15:51:54 +0200

    thermal image
    If any channel left, a black and white raster is generated.
    thermal<-ToRGB(my.raster$band$tirs1, my.raster$band$tirs2)

--- src/main/R/rLandsat8/R/ToRGB.R
+++ src/main/R/rLandsat8/R/ToRGB.R
@@ -1,9 +1,9 @@
 #' creates a raster using 3 landsat8 bands 
-#' @description Creates a raster by any 3 component from Landsat bands.
+#' @description Creates a raster by any 3 component from Landsat bands. If any channel left, a black and white raster is generated
 #'
-#' @param red.component, landsat8 band mapped to the red channel
-#' @param green.component, landsat8 band mapped to the green channel
-#' @param blue.component, landsat8 band mapped to the blue channel
+#' @param red.component, landsat8 band mapped to the red channel. This first channel is mandatory
+#' @param green.component default NULL, landsat8 band mapped to the green channel (optional channel)
+#' @param blue.component default NULL, landsat8 band mapped to the blue channel (optional channel)
 #' @return the combined raster
 #' @examples \dontrun{
 #' filename<-"LC81880342014238LGN00"
@@ -13,35 +13,58 @@
 #'
 #' @export
 
-ToRGB<-function(red.component, green.component, blue.component){
+ToRGB<-function(red.component, green.component = NULL, blue.component = NULL){
      # landsat 8 properties
      landsat8.bitdepth <- 16
      landsat8.colourlevel <- 2 ^ landsat8.bitdepth - 1
      mapping.landasta8colourlevel <- 255 / landsat8.colourlevel
      
      # check band components compatibilities
-     if( (red.component@ncols != green.component@ncols) | (red.component@ncols != blue.component@ncols) | (red.component@nrows != green.component@nrows) | (red.component@nrows != blue.component@nrows))
-          stop("The 3 band components must have the same dimension!")
+     # we always have the red component (at least a channel to get a black & white image)
+     
+     if(!is.null(green.component))
+          if( (red.component@ncols != green.component@ncols) | (red.component@nrows != green.component@nrows))
+               stop("The bands components must have the same dimension!")     
+     
+     if(!is.null(blue.component))
+          if( (red.component@ncols != blue.component@ncols) | (red.component@nrows != blue.component@nrows))
+               stop("The bands components must have the same dimension!")     
      
      # any r/g/b component is suitable for the setting operation
      rgb.datasize <- red.component@ncols * red.component@nrows
      GT   <- GridTopology(c(0, 0), c(1, 1), c(red.component@ncols, red.component@nrows))
      SGDF <- SpatialGridDataFrame(GT, data=data.frame(red = rep(0,times=rgb.datasize), green = rep(0,times=rgb.datasize), blue = rep(0,times=rgb.datasize)))
      
+     # we always have the red component (at least a channel to get a black & white image)
      data.red <- getValues(red.component) * mapping.landasta8colourlevel
      data.red[data.red < 0] <- 0
      data.red[data.red > 255] <- 255
      SGDF$red   <- data.red   * mapping.landasta8colourlevel
      
-     data.green <- getValues(green.component) * mapping.landasta8colourlevel
-     data.green[data.green < 0] <- 0
-     data.green[data.green > 255] <- 255
-     SGDF$green <- data.green
+     if(!is.null(green.component)) {
+          data.green <- getValues(green.component) * mapping.landasta8colourlevel
+          data.green[data.green < 0] <- 0
+          data.green[data.green > 255] <- 255
+          SGDF$green <- data.green
+     }
      
-     data.blue <- getValues(blue.component) * mapping.landasta8colourlevel
-     data.blue[data.blue < 0] <- 0
-     data.blue[data.blue > 255] <- 255
-     SGDF$blue  <- data.blue
+     if(!is.null(blue.component)) {
+          data.blue <- getValues(blue.component) * mapping.landasta8colourlevel
+          data.blue[data.blue < 0] <- 0
+          data.blue[data.blue > 255] <- 255
+          SGDF$blue  <- data.blue
+     }
+     
+     # do a black and white image if any channels left to the list
+     if(is.null(blue.component) | is.null(green.component)){
+          the.color <- round((SGDF$blue + SGDF$green +SGDF$red) / 3)
+          the.color[the.color < 0] <- 0
+          the.color[the.color > 255] <- 255
+          SGDF$blue <- the.color
+          SGDF$green <- the.color
+          SGDF$red <- the.color
+     }
+     
      #image(SGDF, red="red", green="green", blue="blue")
      return(raster(SGDF))
 }
