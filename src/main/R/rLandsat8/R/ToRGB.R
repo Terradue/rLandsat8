719b1ce37ee63e3e1e9f8d686238a0b7a4a0ba8b
Costanzo <costanzo.d@gmail.com>
2014-09-30 15:52:42 +0200

    stack (raster)
    using raster stack to build an rgb raster

--- src/main/R/rLandsat8/R/ToRGB.R
+++ src/main/R/rLandsat8/R/ToRGB.R
@@ -1,10 +1,10 @@
 #' creates a raster using 3 landsat8 bands 
-#' @description Creates a raster by any 3 component from Landsat bands. If any channel left, a black and white raster is generated
+#' @description Creates a raster by any 3 component from Landsat bands.
 #'
 #' @param red.component, landsat8 band mapped to the red channel. This first channel is mandatory
 #' @param green.component default NULL, landsat8 band mapped to the green channel (optional channel)
 #' @param blue.component default NULL, landsat8 band mapped to the blue channel (optional channel)
-#' @return the combined raster
+#' @return the combined raster (stack)
 #' @examples \dontrun{
 #' filename<-"LC81880342014238LGN00"
 #' my.raster <- ReadLandsat8(filename)
@@ -33,38 +33,35 @@ ToRGB<-function(red.component, green.component = NULL, blue.component = NULL){
      # any r/g/b component is suitable for the setting operation
      rgb.datasize <- red.component@ncols * red.component@nrows
      GT   <- GridTopology(c(0, 0), c(1, 1), c(red.component@ncols, red.component@nrows))
-     SGDF <- SpatialGridDataFrame(GT, data=data.frame(red = rep(0,times=rgb.datasize), green = rep(0,times=rgb.datasize), blue = rep(0,times=rgb.datasize)))
+     
+     SGDF.red <- SpatialGridDataFrame(GT, data=data.frame(red = rep(0,times=rgb.datasize)))
+     SGDF.green <- SpatialGridDataFrame(GT, data=data.frame(green = rep(0,times=rgb.datasize)))
+     SGDF.blue <- SpatialGridDataFrame(GT, data=data.frame(blue = rep(0,times=rgb.datasize)))
+     
+     r <- stack()
      
      # we always have the red component (at least a channel to get a black & white image)
      data.red <- getValues(red.component) * mapping.landasta8colourlevel
      data.red[data.red < 0] <- 0
      data.red[data.red > 255] <- 255
-     SGDF$red   <- data.red   * mapping.landasta8colourlevel
+     SGDF.red$red   <- data.red   * mapping.landasta8colourlevel
+     r <- stack(r,raster(SGDF.red))
      
      if(!is.null(green.component)) {
           data.green <- getValues(green.component) * mapping.landasta8colourlevel
           data.green[data.green < 0] <- 0
           data.green[data.green > 255] <- 255
-          SGDF$green <- data.green
+          SGDF.green$green <- data.green
+          r<- stack(r,raster(SGDF.green))
      }
      
      if(!is.null(blue.component)) {
           data.blue <- getValues(blue.component) * mapping.landasta8colourlevel
           data.blue[data.blue < 0] <- 0
           data.blue[data.blue > 255] <- 255
-          SGDF$blue  <- data.blue
+          SGDF.blue$blue <- data.blue
+          r<- stack(r,raster(SGDF.blue)) 
      }
-     
-     # do a black and white image if any channels left to the list
-     if(is.null(blue.component) | is.null(green.component)){
-          the.color <- round((SGDF$blue + SGDF$green +SGDF$red) / 3)
-          the.color[the.color < 0] <- 0
-          the.color[the.color > 255] <- 255
-          SGDF$blue <- the.color
-          SGDF$green <- the.color
-          SGDF$red <- the.color
-     }
-     
-     #image(SGDF, red="red", green="green", blue="blue")
-     return(raster(SGDF))
+
+     return(r)
 }
